8b36be47c45b64aa204100664647ab9664a1f0a7
Antonio Sarosi <sarosiantonio@gmail.com>
2025-06-10 17:48:54 +0200

    Branching

--- src/vm.c
+++ src/vm.c
@@ -80,6 +80,7 @@ static void concatenate() {
 
 static InterpretResult run() {
     #define READ_BYTE() (*vm.ip++)
+    #define READ_SHORT()  (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
     #define READ_CONSTANT() vm.chunk->constants.values[READ_BYTE()]
     #define READ_STRING() AS_STRING(READ_CONSTANT())
 
@@ -244,6 +245,26 @@ static InterpretResult run() {
                 break;
             }
 
+            case OP_JUMP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip += offset;
+                break;
+            }
+
+            case OP_JUMP_IF_FALSE: {
+                uint16_t offset = READ_SHORT();
+                if (is_falsey(peek(0))) {
+                    vm.ip += offset;
+                }
+                break;
+            }
+
+            case OP_LOOP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip -= offset;
+                break;
+            }
+
             case OP_RETURN: {
                 // Exit interpreter.
                 return INTERPRET_OK;
@@ -252,6 +273,7 @@ static InterpretResult run() {
     }
 
     #undef READ_BYTE
+    #undef READ_SHORT
     #undef READ_CONSTANT
     #undef READ_STRING
     #undef BINARY_OP
